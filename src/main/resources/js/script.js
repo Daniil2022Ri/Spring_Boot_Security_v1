bc4bc8e1e63b6d98409b6032c0bb22fc875f9e4a
Darkness_f <143736890+Daniil2022Ri@users.noreply.github.com>
2025-10-05 17:01:38 +0300

    add files

---
+++ src/main/resources/js/script.js
@@ -0,0 +1,8 @@
+
+//Button Action Admin Panel
+const button_Users_table = 'container_btn_action_UserTable'; //Admin Panel button (User table)
+const button_New_User = 'container_btn_action_NewUser'; //Admin Panel button (New User)
+//
+
+const button_edit_user = 'btn_edit'; 
+const button_delete_user = 'btn_delete';
